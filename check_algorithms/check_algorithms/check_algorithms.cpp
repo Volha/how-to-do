8486a005437f6ca81ebb3d24ac8eb20ffa9355a0
volha <baban.volha@gmail.com>
2012-09-28 16:14:21 +0300

    added find()

--- check_algorithms/check_algorithms/check_algorithms.cpp
+++ check_algorithms/check_algorithms/check_algorithms.cpp
@@ -7,12 +7,40 @@
 #include <algorithm>
 using namespace std;
 
-void PrintVector(vector<int>& vectorForFunc)
+void Print(int i)
 {
-	for (int i = 0; i < vectorForFunc.size(); i++)
+	cout << i << "  ";
+}
+
+struct P
+{
+	size_t m_c;
+	P(size_t c) : m_c(c)
 	{
-		cout << vectorForFunc[i] << "  ";
+	
 	}
+	void operator()(int i)
+	{
+		cout << i + m_c << "  ";
+	}
+};
+
+void PrintVector(vector<int>& vectorForFunc)
+{	
+	for_each
+	(
+		vectorForFunc.begin(),
+		vectorForFunc.end(),
+		[](int i)
+		{
+			cout << i << "  ";
+			
+		}
+	);
+	//for (auto ib = vectorForFunc.begin(), ie = vectorForFunc.end(); ib != ie; ++ib)
+	//{
+	//	cout << *ib << "  ";
+	//}
 	cout << "\n";
 };
 
@@ -25,11 +53,11 @@ void CheckResults(bool res)
 {
 	if (res)
 	{
-		 cout << "Right\n";
+		 cout << "right\n";
 	}
 	else 
 	{	
-		cout << "Wrong\n";
+		cout << "wrong\n";
 	}
 }
 
@@ -44,18 +72,48 @@ void TestSort()
 	vectorShouldBe.push_back(15);
 	vectorShouldBe.push_back(90);
 
-	PrintVector(vectorForTest);
-	PrintVector(vectorShouldBe);
-	
 	sort(vectorForTest.begin(), vectorForTest.end());
 	CheckResults(IsEqual(vectorForTest, vectorShouldBe));
 	
-	PrintVector(vectorForTest);
+	vectorForTest.erase(vectorForTest.begin(), vectorForTest.end());
+	vectorForTest.push_back(3);
+	vectorShouldBe.erase(vectorShouldBe.begin(),vectorShouldBe.end());
+	vectorShouldBe.push_back(3);
+
+	sort(vectorForTest.begin(),vectorForTest.end());
+	CheckResults(IsEqual(vectorForTest, vectorShouldBe));
+	
+}
+
+void TestFind()
+{
+	vector<int> vecForTest;
+	vecForTest.push_back(0);
+	vecForTest.push_back(5);
+	vecForTest.push_back(100);
+	vecForTest.push_back(555);
+	cout << "Not found: ";
+	CheckResults((vecForTest.end() == find(vecForTest.begin(), vecForTest.end(), 56)));
+	cout << "The first found value: ";
+	CheckResults((vecForTest.begin()+1 == find(vecForTest.begin(), vecForTest.end(), 5)));
+	vecForTest[0] = 555;
+	vecForTest[1] = 555;
+	vecForTest[2] = 555;
+	vecForTest[3] = 555;
+	cout << "The first found value in set of similar elements: ";
+	CheckResults((vecForTest.begin() == find(vecForTest.begin(), vecForTest.end(), 555)));
+	vecForTest.erase(vecForTest.begin(), vecForTest.end());
+	vecForTest.push_back(4);
+	cout << "The first found value in set of one element: ";
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	argc;argv;
+	cout << "SORT\n";
 	TestSort();
+	cout << "FIND\n";
+	TestFind();
 	cin.get();
 	return 0;
 }
