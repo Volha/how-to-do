ee18dd945c8e6be6ba99ccb1fff9f67c206c5d39
volha <baban.volha@gmail.com>
2012-11-06 17:42:36 +0300

    add weapons

--- Ring/Ring/Ring.cpp
+++ Ring/Ring/Ring.cpp
@@ -4,24 +4,78 @@
 #include "stdafx.h"
 #include "RingClass.h"
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include "Weapon.h"
+#include "GunWeapon.h"
+#include "Bazuka.h"
+#include "RealGunWeapon.h"
+
+template <typename T> 
+RingClass<T> InitializeRing(const std::vector<T>& vec)
+{
+	RingClass<T> r;
+	for(size_t i = 0; i < vec.size(); ++i)
+	{
+		r.AddNode(vec[i]);
+	}
+	return r;
+}
+
+bool CheckRing(RingClass<int>& r, const std::vector<int>& v)
+{
+	bool isEqual = true;
+	for(size_t i = v.size(); i > 0; --i)
+	{
+		if(v[i-1] != r.GetCurrentValue())
+		{
+			isEqual = false;
+			return isEqual;
+		}
+		r.MoveNext();
+	}
+	
+	r.MovePrevious();
+		for(size_t i = 0; i < (v.size()-1); ++i)
+	{
+		if(v[i] != r.GetCurrentValue())
+		{
+			isEqual = false;
+			return isEqual;
+		}
+		r.MovePrevious();
+	}
+	return isEqual;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	RingClass<int> r;
-	r.AddNode(3);
-	r.AddNode(10);
-	r.AddNode(9);
-	r.AddNode(15);
-	r.AddNode(5);
-	while (r.GetCurrentValue() != 3)
+	std::vector<int> v;
+	v.push_back(3);
+	v.push_back(10);
+	v.push_back(9);
+	v.push_back(15);
+	v.push_back(5);
+
+	RingClass<int> ring = InitializeRing(v); 
+	
+
+	if (CheckRing(ring, v))
 	{
-		std::cout << r.GetCurrentValue() << "\n";
-		r.MoveNext();
+		std::cout << "OK" << "\n";
 	}
-	r.RemoveNode();
-	int t = r.GetCurrentValue();
-	r.MoveNext();
-	t = r.GetCurrentValue();
+	std::vector<Weapon*> weapons;
+	weapons.push_back(new GunWeapon("Gun"));
+	weapons.push_back(new RealGunWeapon("Gun"));
+	weapons.push_back(new Bazuka("Gun"));
+
+	RingClass<Weapon*> boonk = InitializeRing(weapons);
+	
+	while(true)
+	{
+		boonk.GetCurrentValue()->Shot();
+		boonk.MoveNext();
+	}
 
 	return 0;
 }
--- Ring/Ring/RingClass.h
+++ Ring/Ring/RingClass.h
@@ -3,6 +3,22 @@
 template <typename T>
 class RingClass
 {
+	class Node
+	{
+	public:
+		Node(const T& val, Node* n = nullptr, Node* pr = nullptr)
+			: value(val)
+			, next(n)
+			, previous(pr)
+		{
+			
+		}
+
+		T value;
+		Node* next;
+		Node* previous;
+	};
+
 public:
 	RingClass()
 		: currentNode(nullptr)
@@ -11,33 +27,51 @@ public:
 			
 	}
 
+	RingClass(const RingClass& r)
+	{
+		Node* p = r.tailNode;
+		currentNode = nullptr;
+		tailNode = nullptr;
+		do
+		{
+			AddNode(p->value);
+			p = p->previous;
+		}
+		while (p != r.tailNode);
+	}
+
+	~RingClass()
+	{
+		while (!IsEmpty())
+		{
+			RemoveNode(currentNode);
+		}
+	}
+
+	bool IsEmpty() const
+	{
+		return currentNode == nullptr;
+	}
+	
 	void AddNode(const T& val)
 	{
 		Node* newNode = new Node(val);
 		if (currentNode == nullptr)
 		{
 			newNode->next = newNode;
+			newNode->previous = newNode;
 			tailNode = newNode;
 		}
 		else 
 		{
 			newNode->next = currentNode;
+			newNode->previous = tailNode;
+			currentNode->previous = newNode;
 			tailNode->next = newNode;
 		}
 		currentNode = newNode;
 	}
-
-	void RemoveNode()
-	{
-		Node* previousNode(currentNode);
-		while (previousNode->next != currentNode)
-		{
-			previousNode = previousNode->next;
-		}
-		previousNode->next = currentNode->next;
-		currentNode = previousNode;
-	}
-		
+	
 	const T& GetCurrentValue() const
 	{
 		return currentNode->value;
@@ -48,20 +82,37 @@ public:
 		currentNode = currentNode->next;
 	}
 
-private:
-	class Node
+	void MovePrevious()
 	{
-	public:
-		Node(const T& val, Node* r = nullptr)
-			: value(val)
-			, next(r)
-		{
-					
-		}
-		T value;
-		Node* next;
-	};
+		currentNode = currentNode->previous;
+	}
 
+private:
+	void RemoveNode(Node* p)
+	{
+		if (p->next == p)
+		{
+			currentNode = nullptr; 
+			tailNode = nullptr;
+		}
+		else
+		{
+			p->previous->next = p->next;
+			p->next->previous = p->previous;
+			if(p == currentNode)
+			{
+				currentNode = p->previous;
+			} 
+			if (p == tailNode)
+			{
+				tailNode = p->previous;
+			}
+		} 
+		delete p;
+	}
+		
+
+private:
 	Node* currentNode;
 	Node* tailNode;
 };
